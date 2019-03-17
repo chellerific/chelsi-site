029a2febd347206c1512198fcfdbc645490ddf44
chellerific <nolascochelsi@gmail.com>
2019-03-17 20:39:29 +0100

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,69 @@
+import PropTypes from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faGem from '@fortawesome/fontawesome-free-regular/faGem';
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      {/*<span className="icon fa-diamond"></span>*/}
+      <FontAwesomeIcon icon={faGem} transform="grow-18" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Chelsi Nolasco</h1>
+        <p>Developer. Artist. Writer.</p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro');
+            }}
+          >
+            Intro
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work');
+            }}
+          >
+            Work
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about');
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact');
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool
+};
+
+export default Header;
